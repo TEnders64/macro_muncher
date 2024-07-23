900582f527d3b77d7a793575d571e1918f38b7ed
TEnders64 <central.district.husky@gmail.com>
2024-07-22 17:46:59 -0700

    initial commit after front-end landing complete

---
+++ macro_muncher/macros/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+from users.models import User
+
+# Create your models here.
+class FoodItem(models.Model):
+    name = models.CharField(max_length=45, db_column='food_name')
+    serving_size = models.IntegerField
+    proteins = models.IntegerField
+    fats = models.IntegerField
+    carbs = models.IntegerField
+    this_serving = models.IntegerField
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Goal(models.Model):
+    LOSE_WEIGHT = 'Lose'
+    MAINTAIN = 'Maintain'
+    GAIN_WEIGHT = 'Gain'
+    GOAL_CHOICES = [
+        (LOSE_WEIGHT, 'Lose Weight'),
+        (MAINTAIN, 'Maintain'),
+        (GAIN_WEIGHT, 'Gain Weight')
+    ]
+
+    goal_type = models.CharField(
+        choices=GOAL_CHOICES,
+        default=None,
+        max_length=12
+    )
+
+    SLOW = 'Slow'
+    RECOMMENDED = 'Recommended'
+    FAST = 'Aggressive'
+    PACE_CHOICES = [
+        (SLOW, 'Slow'),
+        (RECOMMENDED, 'Recommended'),
+        (FAST, 'Aggressive')
+    ]
+
+    goal_pacing = models.CharField(
+        choices=PACE_CHOICES,
+        default=None,
+        max_length=12
+    )
+
+    goal_weight = models.IntegerField(blank=True)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Measurement(models.Model):
+    weight = models.IntegerField
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
